41e61048e253fe1f13f6b1f9bcf03e53c1407ef0
Krayon <krayon.git@qdnx.org>
2016-10-23 18:11:20 +1100

    Minor changes

--- main.c
+++ main.c
@@ -98,33 +98,35 @@ static int usb_deinit(void) {
 static libusb_device_handle *mouse_init(const uint16_t vendor_id, const uint16_t product_id, const char* product_name) {
     if (!usb_ctx) return NULL;
 
-    if (usb_dev_handle) return usb_dev_handle;
-
-    // Look at the mouse based on vendor and usb_device id
-    // FIXME: According to doco (
-    // http://libusb.sourceforge.net/api-1.0/group__dev.html#ga11ba48adb896b1492bbd3d0bf7e0f665
-    // ):
-    // > This function is intended for those scenarios where you are using
-    // > libusb to knock up a quick test application - it allows you to avoid
-    // > calling libusb_get_device_list() and worrying about traversing/freeing
-    // > the list.
-    //
-    // > This function has limitations and is hence not intended for use in real
-    // > applications: if multiple devices have the same IDs it will only give
-    // > you the first one, etc.
-
-    usb_dev_handle = libusb_open_device_with_vid_pid(usb_ctx, vendor_id, product_id);
     if (!usb_dev_handle) {
-        elog("Failed to find %s (%.4x:%.4x)\n", product_name, vendor_id, product_id);
-        return NULL;
+        // Look at the mouse based on vendor and usb_device id
+        // FIXME: According to doco (
+        // http://libusb.sourceforge.net/api-1.0/group__dev.html#ga11ba48adb896b1492bbd3d0bf7e0f665
+        // ):
+        // > This function is intended for those scenarios where you are using
+        // > libusb to knock up a quick test application - it allows you to
+        // > avoid calling libusb_get_device_list() and worrying about
+        // > traversing/freeing the list.
+        //
+        // > This function has limitations and is hence not intended for use
+        // > in real applications: if multiple devices have the same IDs it
+        // > will only give you the first one, etc.
+
+        usb_dev_handle = libusb_open_device_with_vid_pid(usb_ctx, vendor_id, product_id);
+        if (!usb_dev_handle) {
+            elog("Failed to find %s (%.4x:%.4x)\n", product_name, vendor_id, product_id);
+            return NULL;
+        }
+
+        printf("Found %s (%.4x:%.4x) @ 0x%p\n", product_name, vendor_id, product_id, usb_dev_handle);
     }
 
-    printf("Found %s (%.4x:%.4x) @ 0x%p\n", product_name, vendor_id, product_id, usb_dev_handle);
-
-    usb_device = libusb_get_device(usb_dev_handle);
     if (!usb_device) {
-        elog("ERROR: Failed to retrieve usb_device info @ 0x%p\n", usb_dev_handle);
-        return NULL;
+        usb_device = libusb_get_device(usb_dev_handle);
+        if (!usb_device) {
+            elog("ERROR: Failed to retrieve usb_device info @ 0x%p\n", usb_dev_handle);
+            return NULL;
+        }
     }
 
     // Get usb_device descriptor
@@ -155,6 +157,7 @@ static int mouse_deinit(void) {
     // Finish up with mouse
     if (usb_dev_handle) libusb_close(usb_dev_handle);
     usb_dev_handle = NULL;
+    usb_device     = NULL;
 
     return 1;
 }
